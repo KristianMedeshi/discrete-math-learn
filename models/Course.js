3f3ee10e5dc36811be673b313a210f5a5b65be09
KristianMedeshi <kristianmedeshi@gmail.com>
2023-11-12 10:36:30 +0200

    fix typo

--- models/Course.js
+++ models/Course.js
@@ -1,12 +1,12 @@
 const { mongoose, Schema } = require('mongoose');
 
 const courseSchema = new Schema({
-  title: { type: String, require: true },
-  description: { type: String, require: true },
-  price: { type: Number, require: true },
-  teachers: { type: [String], require: true },
-  difficulty: { type: String, require: true },
-  duration: { type: Number, require: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  teachers: { type: [String], required: true },
+  difficulty: { type: String, required: true },
+  duration: { type: Number, required: true },
   content: { type: Schema.Types.ObjectId, ref: 'CourseContent', required: true },
 });
 
