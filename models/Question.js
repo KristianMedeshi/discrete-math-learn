992dab0dc426c04e5f5bfdb3fd388de3c19ee5ba
MyhtonIvan <mykhtoniuk.ivan@student.uzhnu.edu.ua>
2023-11-12 10:35:33 +0200

    add timestamps and tags validation

--- models/Question.js
+++ models/Question.js
@@ -1,10 +1,19 @@
 const { mongoose, Schema } = require('mongoose');
+const validTags = require('../constants/tags');
 
-const questionSchema = new Schema({
-  title: { type: String, require: true },
-  tags: { type: [String], default: undefined },
-  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  answers: { type: Schema.Types.ObjectId, ref: 'Answers', required: true },
-});
+const questionSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    tags: {
+      type: [String],
+      enum: validTags,
+      default: [],
+    },
+    authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    details: { type: String, required: true },
+    answers: { type: Schema.Types.ObjectId, ref: 'Answers', required: true },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Question', questionSchema);
