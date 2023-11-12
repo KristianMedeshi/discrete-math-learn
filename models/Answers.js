526f59d99c10303f78e5d0865e387c6418b6053a
MyhtonIvan <mykhtoniuk.ivan@student.uzhnu.edu.ua>
2023-11-12 10:36:03 +0200

    add timestamps to answers

--- models/Answers.js
+++ models/Answers.js
@@ -2,12 +2,14 @@ const { mongoose, Schema } = require('mongoose');
 
 const answersSchema = new Schema({
   answers: [{
-    type: {
-      answer: { type: String, require: true },
-      author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-      isSolution: { type: Boolean, default: Boolean(false) },
-    },
-    default: [],
+    type: new Schema(
+      {
+        answer: { type: String, required: true },
+        authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        isSolution: { type: Boolean, default: Boolean(false) },
+      },
+      { timestamps: true },
+    ),
   }],
 });
 
