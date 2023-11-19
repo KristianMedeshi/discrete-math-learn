763917483e98418fbbf6050e2cf08954cbe17d77
MyhtonIvan <mykhtoniuk.ivan@student.uzhnu.edu.ua>
2023-11-19 21:17:23 +0200

    allow get courses for unauthorized users

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const authMiddleware = require('../middleware/authMiddleware');
 const uploadMiddleware = require('../middleware/uploadMiddleware');
 const coursesController = require('../controllers/coursesController');
 
 const router = Router();
 
 router.get('/', coursesController.getCourses);
-router.post('/', uploadMiddleware.single('file'), coursesController.createCourse);
+router.post('/', authMiddleware, uploadMiddleware.single('file'), coursesController.createCourse);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.use('/', usersRoutes);
 router.use('/forum', authMiddleware, forumRoutes);
-router.use('/courses', authMiddleware, coursesRoutes);
+router.use('/courses', coursesRoutes);
 router.post('/upload', uploadMiddleware.single('file'), (req, res) => {
   const filePath = `${req.protocol}://${req.get('host')}/${req.file.path}`;
   res.status(201).send({ message: 'File uploaded successfully', path: filePath });
