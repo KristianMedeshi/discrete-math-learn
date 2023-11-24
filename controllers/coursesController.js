14addbb05a9271c9b99807c0de3ce4817efac74c
MyhtonIvan <mykhtoniuk.ivan@student.uzhnu.edu.ua>
2023-11-24 21:50:30 +0200

    add get course request

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,14 +1,15 @@
 const Course = require('../models/Course');
-const difficultyLevels = require('../constants/difficultyLevels');
+const CourseBlock = require('../models/CourseBlock');
+const validLevels = require('../constants/validLevels');
 const getFullPath = require('../utils/getFullPath');
 
 module.exports.getCourses = async (req, res) => {
   try {
     const {
-      skip, limit, difficulties, durationRange,
+      skip, limit, levels, durationRange,
     } = req.query;
     const filter = {
-      difficulty: { $in: difficulties ?? difficultyLevels },
+      level: { $in: levels ?? validLevels },
       duration: {
         $gte: durationRange?.[0] ?? 0,
         $lte: durationRange?.[1] ?? Infinity,
@@ -37,17 +38,30 @@ module.exports.createCourse = async (req, res) => {
   try {
     const author = req.user.id;
     const {
-      name, description, teachers, price, duration, difficulty,
+      name, description, instructors, price, duration, level,
     } = req.body;
     if (!req?.file) {
       req.file = { path: 'uploads/1700295692126.jpeg' };
     }
     const image = req.file.path;
     const newCourse = new Course({
-      author, name, description, teachers, price, duration, difficulty, image,
+      author, name, description, instructors, price, duration, level, image,
     });
     await newCourse.save();
-    res.status(201).json({ message: 'Course has been created successfully' });
+    res.status(201).json({ message: 'Course has been created successfully', id: newCourse._id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error', details: error.message });
+  }
+};
+
+module.exports.getCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const course = await Course.findById(id);
+    const blocks = await CourseBlock.find({ course: id }).select('title');
+    course.image = getFullPath(req, course.image);
+    res.status(200).json({ course, blocks });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error', details: error.message });
